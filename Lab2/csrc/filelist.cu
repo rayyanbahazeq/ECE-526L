9c2aa78680e9247acc12545ee1cea28cb4c415a5
Abdul Rahman Bin Ali Bahazeq <ab192644@dcd154.csun.edu>
2023-02-16 21:26:06 -0800

    lab2

--- Lab2/csrc/filelist.cu
+++ Lab2/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _31594_archive_1.so
-_31594_archive_1.so : archive.3/_31594_archive_1.a
+ARCHIVE_OBJS += _2176_archive_1.so
+_2176_archive_1.so : archive.6/_2176_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_31594_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_2176_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_31594_archive_1.so $@
+	@ln -sf .//../simv.daidir//_2176_archive_1.so $@
 
 
 
