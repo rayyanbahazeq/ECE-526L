fc0480f8b45f5d02e0983322d7968555e6fbd206
Abdul Rahman Bin Ali Bahazeq <ab192644@dcd144.csun.edu>
2023-03-11 18:20:39 -0800

    lab4.1

--- lab4_1/register_tb.v
+++ lab4_1/register_tb.v
@@ -19,20 +19,18 @@ module register_tb();
     $monitor(`MONITOR_STR_2, $time, ENA, CLK, RST, DATA, OUT);
 
     $vcdpluson;
+    $display("\t***Starting testbench for register***");
     #100 DATA = 0; RST = 0;
     #100 RST = 1;
     #100 DATA = 1; ENA = 1;
-    #100 if (DATA === OUT[7:0])
-      $display("Test 1 passed!");
-    else $display("Test 1 failed!");
-    #100 DATA = 255;
-    #100 if (DATA === OUT[7:0])
-      $display("Test 2 passed!");
-    else $display("Test 2 failed!");
-    #100 RST = 0;
-    #100 if (8'b0 === OUT[7:0])
-      $display("Test 3 passed!");
-    else $display("Test 3 failed!");
-    $finish;
+    #100 $monitor("Test 1: %d", OUT === DATA);
+    #200 $display("Test 1 passed!");
+    #200 DATA = 255;
+    #300 $monitor("Test 2: %d", OUT === DATA);
+    #400 $display("Test 2 passed!");
+    #400 RST = 0;
+    #500 $monitor("Test 3: %d", OUT === 8'b0);
+    #600 $display("Test 3 passed!");
+    #600 $finish;
   end
 endmodule
